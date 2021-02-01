6eb7581fec469e51683aed8c5c33b031ac105429
Justin Keller <kellerjustin@gmail.com>
2021-02-01 13:54:56 -0600

    adds lab and radiology appts

--- schedulers/jobs.py
+++ schedulers/jobs.py
@@ -51,7 +51,7 @@ def dictfetchall(days_in_advance):
            join (select max(sch5apptstat_id) as id, sch5apptstat_apptid from scheduling_appointment_status group by sch5apptstat_apptid) as ss on ss.sch5apptstat_apptid = sch5appt_id
            join scheduling_appointment_status on sch5apptstat_id = ss.id
     WHERE  Date(sch5appt_datetime) = {}
-    	   and sch5task_owner_dept in (34,47)
+    	   and sch5task_owner_dept in (34,47,49,31)
            and sch5apptstat_code = 'S'
     ORDER  BY sch5appt_datetime
     """
